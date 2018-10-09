db5aecc99247a5b021849babfde1c966be311fc6
Geek05 <bharathakumar3@gmail.com>
2018-10-09 12:14:14 +0530

    added new REST API to REST API testing package

--- src/test/java/org/test/rest/RESTAPITest.java
+++ src/test/java/org/test/rest/RESTAPITest.java
@@ -5,6 +5,10 @@ import org.apache.logging.log4j.Logger;
 import org.test.mobile.AppiumTest;
 import org.testng.annotations.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
 public class RESTAPITest {
 	
 	private static Logger log = LogManager.getLogger(RESTAPITest.class.getName());
@@ -20,6 +24,24 @@ public class RESTAPITest {
 	public void testTwitter() {
 		log.info("Test Twitter");
 		System.out.println("Test Twitter");
+	}
+	
+	@Test
+	public void testOpenRestAPI() {
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+		Response response = RestAssured.given().
+							when().
+							get("/todos/1").
+							then().extract().response();
+		System.out.println(response.asString());
+		
+		System.out.println();
+		JsonPath jsonPath = new JsonPath(response.asString());
+		System.out.println("userId: "+jsonPath.get("userId").toString());
+		System.out.println("Id: "+jsonPath.get("id").toString());
+		System.out.println("title: "+jsonPath.get("title").toString());
+		System.out.println("completed: "+jsonPath.get("completed").toString());
+		System.out.println();
 	}
 
 }
